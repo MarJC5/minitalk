ab2812e0cbe7e7905b7167b3ebff5df97e8e6d09
MarJC5 <jmartin@student.42lausanne.ch>
2021-12-01 17:14:42 +0100

    Update

---
+++ minitalk/src/minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmartin <jmartin@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/01 16:39:42 by jmartin           #+#    #+#             */
+/*   Updated: 2021/12/01 17:12:29 by jmartin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <signal.h>
+
+#endif
